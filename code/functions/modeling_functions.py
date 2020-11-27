db898b8eea6c4893dadf539555d70a413fff4344
Sara Price <saraprice@Saras-MacBook-Air.local>
2020-11-27 17:49:16 -0500

    adding in model evaluation functions comb_auc_recall and EV_binary

--- code/functions/modeling_functions.py
+++ code/functions/modeling_functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.metrics import auc, roc_curve
 def select_features_corr_imp(df, feat_import, rho_cutoff, n_features):
     '''
     Purpose:
@@ -41,3 +41,63 @@ def select_features_corr_imp(df, feat_import, rho_cutoff, n_features):
     #final full correlation dataframe for sanity check
     output_corr_df = df[selected_features].corr()
     return selected_features, output_corr_df
+
+
+def comb_auc_recall(y_true, y_proba, y_preds, recall_weight):
+    '''
+    Purpose: Get a combined evalution of AUC and recall that weights recall a bit more heavily since we care the most 
+    about accurately identifying fires
+    Inputs:
+    ------
+        y_true:         numpy array
+                        array of actual binary y values in test set
+        y_proba:        numpy array
+                        probability predictions from model
+        y_preds:        numpy array
+                        binary predictions from model
+        recall_weight : float
+                        weight for recall value
+                 
+    Returns:
+    --------
+        weighted average of AUC and recall
+    
+    '''
+    fpr, tpr, thresholds = roc_curve(y_true, y_proba)
+    roc_auc = auc(fpr, tpr)
+    recall = recall_score(y_true, y_preds)
+    return (roc_auc + 1.25*recall)/2
+
+
+
+def EV_binary(y_true, y_preds, V_tp, V_tn, C_fp, C_fn):
+    '''
+    Purpose: Generate cost sensitive expected value for binary classification of wildfires
+    -------
+    Inputs:
+    ------
+        y_true:  numpy array
+                 array of actual binary y values in test set
+        y_preds: numpy array
+                 predictions from model
+        V_tp :   float
+                 value of a true positive 
+        V_tn :   float
+                 value of a true negative
+        C_fp :   float
+                 cost assigned to a false positive (i.e. predicting a wildfire when there is one)
+        C_fn :   float
+                 cost assigned to a false negative (i.e. predicting no wildfire when there is one)
+    Returns:
+    -------
+        EV :     float
+                 expected value for model
+    '''
+    #Geneate probabilities of wildfires i.e. rate of wildfires in the test set 
+    P_1 = np.sum(y_true==1)/y_true.shape[0]
+    P_0 = np.sum(y_true==0)/y_true.shape[0]
+    cm = pd.DataFrame(confusion_matrix(y_true, y_preds, normalize = 'true'), columns = ['0', '1'], index = ['0','1'])
+    TPR = cm.iloc[1,1]
+    FPR = cm.iloc[0,1]
+    EV = P_1*(TPR*V_tp + (1-TPR)*C_fn) + P_0*(FPR*C_fp + (1 - FPR)*V_tn)
+    return EV
